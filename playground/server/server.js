65c34f4252283e11c563283deaef5a93c560dde4
Aman <aman9verma@hotmail.com>
2017-09-18 03:29:11 +0530

    Add GET /todos/:id

--- playground/server/server.js
+++ playground/server/server.js
@@ -1,6 +1,7 @@
 // server.js is just going to be used for creating routes.
 const express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo');
@@ -29,7 +30,20 @@ app.get('/todos', (req, res) => {
   });
 });
 
-
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id; // params contains key value pairs
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  Todo.findById(id).then((doc) => { //success callback
+    if (!doc) {
+      return res.status(404).send();
+    }
+    res.status(200).send({doc});
+  }).catch((e) => {
+    res.status(400).send();
+  })
+}) // url parameters
 
 
 app.listen(3000, () => {
